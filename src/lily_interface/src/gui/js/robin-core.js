1af92b491cca944326b6f5f912bc8ba77d335cd8
Simon Castle-Green <simon.castle-green2@nottingham.ac.uk>
2022-03-15 11:06:01 +0000

    Initial Web interface for WOZ

--- src/lily_interface/src/gui/js/robin-core.js
+++ src/lily_interface/src/gui/js/robin-core.js
@@ -1,6 +1,6 @@
 
 var ros = new ROSLIB.Ros({
-    url : 'ws://localhost:9090'
+    url : 'ws://10.25.132.128:9090'
 });
 
 ros.on('connection', function() {
@@ -15,75 +15,23 @@ ros.on('close', function() {
     console.log('Connection to websocket server closed.');
 });
 
-var cmdPaddleReset = new ROSLIB.Topic({
+var speechText = new ROSLIB.Topic({ 
     ros : ros,
-    name : '/paddle_1/reset_centre',
-    messageType : 'std_msgs/Bool'
-})
-
-var pdl1_reset_msg = new ROSLIB.Message({data:true})
-cmdPaddleReset.publish(pdl1_reset_msg)
-
-var listener = new ROSLIB.Topic({
-    ros : ros,
-    name : '/client_count',
-    messageType : 'std_msgs/Int32'
+    name : '/speech',
+    messageType : 'std_msgs/String'
 });
 
-listener.subscribe(function(message) {
-    $('.client_count').html(message.data);
-    console.log('Received message on ' + listener.name + ': ' + message.data);
+$('.publish-speech').on('click', function(){
+    speak($('.custom-text-to-speak').val());
 });
 
-var listener_player1_score = new ROSLIB.Topic({
-    ros : ros,
-    name : '/player_1/score',
-    messageType : 'std_msgs/Int32'
+$('.quick-speak').on('click', function(){
+    speak($(this).html());
 });
 
-listener_player1_score.subscribe(function(message) {
-    $('.player_1_score').html(message.data);
-});
+function speak(message) {
+    speechText.publish(new ROSLIB.Message({data:message}));
+}
 
-var listener_player2_score = new ROSLIB.Topic({
-    ros : ros,
-    name : '/player_2/score',
-    messageType : 'std_msgs/Int32'
-});
+console.log('Script running');
 
-listener_player2_score.subscribe(function(message) {
-    $('.player_2_score').html(message.data);
-});
-
-
-var battery_paddle_1 = new ROSLIB.Topic({
-    ros : ros,
-    name : '/paddle_1/battery_state',
-    messageType : 'sensor_msgs/BatteryState'
-});
-
-battery_paddle_1.subscribe(function(message) {
-    $('.battery-paddle-1').html(message.data.percentage);
-});
-
-
-var battery_paddle_2 = new ROSLIB.Topic({
-    ros : ros,
-    name : '/paddle_2/battery_state',
-    messageType : 'sensor_msgs/BatteryState'
-});
-
-battery_paddle_2.subscribe(function(message) {
-    $('.battery-paddle-2').html(message.data.percentage);
-});
-
-
-var battery_ball = new ROSLIB.Topic({
-    ros : ros,
-    name : '/ball/battery_state',
-    messageType : 'sensor_msgs/BatteryState'
-});
-
-battery_ball.subscribe(function(message) {
-    $('.battery-ball').html(message.data.percentage);
-});
