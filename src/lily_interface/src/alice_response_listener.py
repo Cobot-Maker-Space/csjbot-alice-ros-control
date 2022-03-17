6112b5d6f0768871a76da82e27e08237d995c95d
Simon Castle-Green <simon.castle-green2@nottingham.ac.uk>
2022-03-17 15:29:21 +0000

    Adding missing rosnode for listening

---
+++ src/lily_interface/src/alice_response_listener.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from handlers.listener import SocketListenerHandler
+import rospy 
+
+class LilySocketListener(object):
+
+    def __init__(self):
+
+        rospy.init_node('alice_listener')
+
+        self.socket_listener = SocketListenerHandler()
+        self.socket_listener.start_listening()
+    
+if __name__ == '__main__':
+    LilySocketListener()
