56c392b7981c4dcac2e6a7a9f64e8feef7ed38b5
Simon Castle-Green <simon.castle-green2@nottingham.ac.uk>
2022-07-19 15:37:29 +0100

    Fixed message call

--- src/csjbot_alice/src/subscriber_parts_cv.py
+++ src/csjbot_alice/src/subscriber_parts_cv.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from socket import timeout
 from handlers.video import VideoOptionsHandler
 import rospy
 import cv2
@@ -16,11 +17,11 @@ class LilyPartsRecognition(object):
         rospy.spin()
     
     def process_image(self, msg):
-        image_msg = rospy.wait_for_message("/camera/image")
+        image_msg = rospy.wait_for_message("/camera/image", Empty, timeout=5)
 
         bridge = CvBridge()
-        orig = bridge.imgmsg_to_cv2(image_msg, "bgr8")
-        cv2.imwrite("parts.jpg", orig)
+        original_image = bridge.imgmsg_to_cv2(image_msg, "bgr8")
+        cv2.imwrite("parts_on_tray.jpg", original_image)
        
 if __name__ == '__main__':
     LilyPartsRecognition()
